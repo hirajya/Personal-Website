cac4df18e779ced65c99bb450d6e3d5e2e31ddd9
hirajya <rodneyestrada2425@gmail.com>
2025-03-10 23:40:19 +0800

    about me lei

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
           quality={100}
         />
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <h1 className="text-white text-5xl font-bold">About Me</h1>
+          <h1 className="text-white text-5xl font-bold">About Me, Lei</h1>
         </div>
       </div>
 
